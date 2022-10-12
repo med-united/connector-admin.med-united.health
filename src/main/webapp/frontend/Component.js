206448975ff42afa9c0f4234b8cb7fac02e0a5b7
simoneS96 <simone.stifano@incentergy.de>
2022-10-12 13:18:11 +0200

    make rows clickable

--- src/main/webapp/frontend/Component.js
+++ src/main/webapp/frontend/Component.js
@@ -1,5 +1,7 @@
-sap.ui.define([ "sap/ui/core/UIComponent", "sap/ui/model/odata/v2/ODataModel"],
-function(UIComponent, ODataModel) {
+sap.ui.define([
+"sap/ui/core/UIComponent", "sap/ui/model/odata/v2/ODataModel",
+"sap/f/FlexibleColumnLayoutSemanticHelper",],
+function(UIComponent, ODataModel, FlexibleColumnLayoutSemanticHelper) {
 	"use strict";
 	return UIComponent.extend("frontend.Component", {
 
@@ -13,6 +15,19 @@ function(UIComponent, ODataModel) {
 			// create the views based on the url/hash
 			this.getRouter().initialize();
 
-		}
+		},
+
+		getHelper: function () {
+            const oFCL = this.getRootControl().byId("fcl"),
+            oParams = jQuery.sap.getUriParameters(),
+            oSettings = {
+                defaultTwoColumnLayoutType: sap.f.LayoutType.TwoColumnsMidExpanded,
+                defaultThreeColumnLayoutType: sap.f.LayoutType.ThreeColumnsMidExpanded,
+                mode: oParams.get("mode"),
+                initialColumnsCount: oParams.get("initial"),
+                maxColumnsCount: oParams.get("max")
+            };
+            return FlexibleColumnLayoutSemanticHelper.getInstanceFor(oFCL, oSettings);
+        }
 	});
 });
--- src/main/webapp/frontend/controller/App.controller.js
+++ src/main/webapp/frontend/controller/App.controller.js
@@ -4,5 +4,15 @@ sap.ui.define([
    "use strict";
    return Controller.extend("frontend.controller.App", {
 
+        onListItemPress: function (oEvent) {
+                    var oNextUIState = this.getOwnerComponent().getHelper().getNextUIState(1),
+                        entityPath = oEvent.getSource().getBindingContext().getPath(),
+                        entity = entityPath.split("/").slice(-1).pop();
+
+                    var oParams = {layout: oNextUIState.layout};
+                    oParams[this.getEntityName().toLowerCase()] = entity;
+                    this.oRouter.navTo(this.getEntityName().toLowerCase() + "-detail", oParams);
+                },
+
    });
 });
