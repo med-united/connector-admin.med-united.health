5eaac9fc12137393ed99cba2b3d4595ace567e20
simoneS96 <simone.stifano@incentergy.de>
2022-11-28 10:42:50 +0100

    update cors allow origin to make it work with basic auth

--- src/main/java/health/medunited/architecture/CORSFilter.java
+++ src/main/java/health/medunited/architecture/CORSFilter.java
@@ -7,9 +7,10 @@ import javax.ws.rs.ext.Provider;
 
 @Provider
 public class CORSFilter implements ContainerResponseFilter {
+
     @Override
     public void filter(final ContainerRequestContext requestContext, final ContainerResponseContext responseContext) {
-        responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", "http://localhost:8081");
         responseContext.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
         String reqHeader = requestContext.getHeaderString("Access-Control-Request-Headers");
 
