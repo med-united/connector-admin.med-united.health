8157fada88f59630bbbda88b7e23a2ef4f384312
Pablo Escaida <1pablo.escaida1@gmail.com>
2023-03-10 17:56:57 +0100

    Fixed readCardCertificate Now it is reading into the Connector Admin Tool

--- src/main/webapp/frontend/controller/Detail.controller.js
+++ src/main/webapp/frontend/controller/Detail.controller.js
@@ -64,7 +64,7 @@ sap.ui.define([
 
             this.getView().getModel("Cards").loadData("connector/event/get-cards", {}, "true", "GET", false, true, mHeaders);
 
-            this.getView().getModel("Certificates").loadData("connector/certificates/readCardCertificate", {}, "true", "GET", false, true, mHeaders);
+            this.getView().getModel("Certificates").loadData("connector/certificate/readCardCertificate?CardHandle=HBA-67", {}, "true", "GET", false, true, mHeaders);
 
             this.getView().getModel("CardTerminals").loadData("connector/event/get-card-terminals", {}, "true", "GET", false, true, mHeaders);
 
