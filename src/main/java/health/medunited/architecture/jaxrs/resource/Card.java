8d04eab8d89fa9daa091c1bd147b8af800545127
Timon Wagener <timon.wagener@incentergy.de>
2023-03-16 13:10:46 +0100

    using getter and setter to create JSON Object

--- src/main/java/health/medunited/architecture/jaxrs/resource/Card.java
+++ src/main/java/health/medunited/architecture/jaxrs/resource/Card.java
@@ -2,41 +2,31 @@ package health.medunited.architecture.jaxrs.resource;
 
 import static health.medunited.architecture.provider.ContextTypeProducer.copyValuesFromProxyIntoContextType;
 
-import java.lang.reflect.Array;
 import java.math.BigInteger;
-import java.util.GregorianCalendar;
-import java.util.List;
-import java.util.TimeZone;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.json.bind.Jsonb;
-import javax.json.bind.JsonbBuilder;
+import javax.json.Json;
+import javax.json.JsonObject;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
-import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.datatype.DatatypeFactory;
-import javax.xml.datatype.XMLGregorianCalendar;
+
 import javax.xml.ws.Holder;
 
-import com.google.gson.Gson;
+
 import de.gematik.ws.conn.cardservice.v8.CardInfoType;
-import de.gematik.ws.conn.cardservice.v8.GetPinStatusResponse;
 import de.gematik.ws.conn.cardservice.v8.PinStatusEnum;
 import de.gematik.ws.conn.cardservice.wsdl.v8.CardServicePortType;
 import de.gematik.ws.conn.cardservicecommon.v2.CardTypeType;
 import de.gematik.ws.conn.cardservicecommon.v2.PinResultEnum;
-import de.gematik.ws.conn.certificateservice.v6.ReadCardCertificate;
-import de.gematik.ws.conn.certificateservice.v6.ReadCardCertificateResponse;
-import de.gematik.ws.conn.certificateservice.v6.VerifyCertificateResponse;
-import de.gematik.ws.conn.certificateservicecommon.v2.CertRefEnum;
-import de.gematik.ws.conn.certificateservicecommon.v2.X509DataInfoListType;
 import de.gematik.ws.conn.connectorcommon.v5.Status;
 import de.gematik.ws.conn.connectorcontext.v2.ContextType;
 import de.gematik.ws.conn.cardservice.wsdl.v8.FaultMessage;
@@ -44,7 +34,6 @@ import de.gematik.ws.conn.eventservice.v7.GetCards;
 import de.gematik.ws.conn.eventservice.v7.GetCardsResponse;
 import de.gematik.ws.conn.eventservice.wsdl.v7.EventServicePortType;
 import health.medunited.architecture.jaxrs.resource.model.PINStatus;
-import health.medunited.architecture.jaxrs.resource.model.VerifyAllEntry;
 
 @RequestScoped
 @Path("card")
@@ -103,20 +92,31 @@ public class Card {
     }
 
     @GET
-    @Path("/PINStatus")
-    public List<PINStatus> AllPinStatus() throws Throwable {
+    @Path("/pinStatus")
+    public Collection<JsonObject> AllPinStatus() throws Throwable {
         GetCards getCards = new GetCards();
         getCards.setContext(copyValuesFromProxyIntoContextType(contextType));
 
         GetCardsResponse getCardResoponse = eventServicePortType.getCards(getCards);
         List<PINStatus> result = getCardResoponse.getCards().getCard().stream().map(this::getPINStatus).collect(Collectors.toList());
-        return result;
+        Collection<JsonObject> items = new ArrayList<JsonObject>();
+        for (PINStatus pinStatus : result){
+            JsonObject value = Json.createObjectBuilder()
+                    .add("cardHandle", pinStatus.getHandle())
+                    .add("cardType", pinStatus.getType())
+                    .add("status", pinStatus.getStatus()).build();
+            items.add(value);
+        }
+        return items;
     }
 
     private PINStatus getPINStatus(CardInfoType cardInfoType) {
 
         PINStatus pinStatus = new PINStatus();
         pinStatus.setCardInfoType(cardInfoType);
+
+        pinStatus.setHandle(cardInfoType.getCardHandle().toString());
+        pinStatus.setType(cardInfoType.getCardType().toString());
 
         String pinType = "";
 
