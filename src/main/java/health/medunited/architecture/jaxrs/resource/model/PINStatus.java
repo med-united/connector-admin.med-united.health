32b3e5155e906eb4b469641a40fa2d4e11416b8f
Timon Wagener <timon.wagener@incentergy.de>
2023-03-16 13:11:20 +0100

    Defining getter and setter

--- src/main/java/health/medunited/architecture/jaxrs/resource/model/PINStatus.java
+++ src/main/java/health/medunited/architecture/jaxrs/resource/model/PINStatus.java
@@ -1,13 +1,16 @@
 package health.medunited.architecture.jaxrs.resource.model;
 
 import de.gematik.ws.conn.cardservice.v8.CardInfoType;
-import de.gematik.ws.conn.cardservice.v8.GetPinStatusResponse;
 
 public class PINStatus {
 
     CardInfoType cardInfoType;
 
     String status;
+
+    String handle;
+
+    String type;
 
     public void setCardInfoType(CardInfoType cardInfoType) {
         this.cardInfoType = cardInfoType;
@@ -16,4 +19,14 @@ public class PINStatus {
     public void setStatus(String status){
         this.status = status;
     }
+
+    public String getStatus(){return this.status;}
+
+    public void setHandle(String handle){ this.handle = handle;}
+
+    public String getHandle(){return this.handle;}
+
+    public void setType(String type) { this.type = type;}
+
+    public String getType(){return this.type;}
 }
