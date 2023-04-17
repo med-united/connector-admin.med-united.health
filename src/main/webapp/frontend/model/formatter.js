6aee69d1708fea22aa16e37e5e20d4ee7f4fbce1
Pablo Escaida <1pablo.escaida1@gmail.com>
2023-04-17 13:18:17 +0200

    CAT-59: Formatting both timestamp columns correctly in human readable format

--- src/main/webapp/frontend/model/formatter.js
+++ src/main/webapp/frontend/model/formatter.js
@@ -2,8 +2,20 @@ sap.ui.define([], function () {
 	"use strict";
 	return {
 		formatDateAndTimeWithTimezone: function (sStatus) {
-			var resourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
-		    return "2004";
+		    let dateAndTime = sStatus.split("T");
+		    let datePart = dateAndTime[0].split("-");
+		    let timePart =  dateAndTime[1];
+
+
+
+		    let year = datePart[0];
+		    let month = datePart[1];
+		    let day = datePart[2];
+		    let timeOnly = timePart.split(".")[0].split("Z")[0];
+
+		    return day+"."+month+"."+year+" "+timeOnly;
+
+
 		}
 	};
 });
