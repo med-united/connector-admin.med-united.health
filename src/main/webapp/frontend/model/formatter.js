2f2fa2da9e4db1f267b8a5c1658381390a207fe6
Pablo Escaida <1pablo.escaida1@gmail.com>
2023-04-17 13:19:49 +0200

    CAT-59: Removing whitespaces

--- src/main/webapp/frontend/model/formatter.js
+++ src/main/webapp/frontend/model/formatter.js
@@ -6,16 +6,12 @@ sap.ui.define([], function () {
 		    let datePart = dateAndTime[0].split("-");
 		    let timePart =  dateAndTime[1];
 
-
-
 		    let year = datePart[0];
 		    let month = datePart[1];
 		    let day = datePart[2];
 		    let timeOnly = timePart.split(".")[0].split("Z")[0];
 
 		    return day+"."+month+"."+year+" "+timeOnly;
-
-
 		}
 	};
 });
