41d8dcd071dcd6e68b245f6c2568ef2bf76b7e0d
TimonWag <97670643+TimonWag@users.noreply.github.com>
2023-05-02 14:47:58 +0200

    CAT-75 Added second ListCard

--- src/main/webapp/dashboard/controller/Dashboard.controller.js
+++ src/main/webapp/dashboard/controller/Dashboard.controller.js
@@ -15,12 +15,15 @@ sap.ui.define(
           this._bDescendingSort = false;
           const oConnectorList = new JSONModel();
           this.getView().setModel(oConnectorList, "Connectors");
+          const oCardList = new JSONModel();
+          this.getView().setModel(oCardList, "Cards");
           this.setCardList();
         },
 
         setCardList: function(){
              const runtimeConfigModel = new sap.ui.model.odata.v2.ODataModel("../Data.svc",true);
              const oConnectorList = this.getView().getModel("Connectors");
+             const oCardList = this.getView().getModel("Cards");
              let url = document.URL;
              url = url.endsWith("#") ? url.substring(0, url.length-1) : url.substring(0, url.length);
              runtimeConfigModel.read("/RuntimeConfigs", {
@@ -32,6 +35,9 @@ sap.ui.define(
                  let inactiveTerminals = 0;
                  let activeConnectors= 0;
                  let activeTerminals = 0;
+                 let invalidSMCB = 0;
+                 let invalidHBA = 0;
+                 const cardTypes = {"SMC_KT" : 0, "SMC_B" : 0, "HBA": 0, "EGK": 0, "KVK": 0}
                  const numConfigs = configs.length;
                  let numResponses = 0;
                  configs.forEach(function (config) {
@@ -47,7 +53,9 @@ sap.ui.define(
                    Promise.all([
                      fetch("connector/event/get-card-terminals", {headers : getCardsHeaders}),
                      fetch("connector/event/get-cards", {headers : getCardsHeaders}),
-                     fetch("dashboard/resources/ConnectorList.json")
+                     fetch("dashboard/resources/ConnectorList.json"),
+                     fetch("connector/card/pinStatus", {headers : getCardsHeaders}),
+                     fetch("dashboard/resources/CardsCard.json")
                    ]).then(function (responses) {
                      return Promise.all(responses.map(function (response) {
                        if(response.ok){
@@ -65,6 +73,19 @@ sap.ui.define(
                           let cards = data[1].cards.card;
                           numCards = numCards + cards.length;
                           activeConnectors++;
+                          const pinStatus = data[3]
+                          for(let j = 0; j < pinStatus.length; j++)
+                          {
+                            const cardType = pinStatus[j].cardType;
+                            cardTypes[cardType] = cardTypes[cardType] + 1;
+                            const status = pinStatus[j].status;
+                            if(cardType == "SMC_B" && status!="VERIFIED"){
+                                invalidSMCB++;
+                            }
+                            if(cardType == "HBA" && status!="VERIFIED"){
+                                invalidHBA++;
+                            }
+                          }
                         }
                         else{
                            inactiveConnectors++;
@@ -74,30 +95,70 @@ sap.ui.define(
                            const statusTerminals = inactiveTerminals > 0 ? "Error" : "Success";
                            const infoConnectors = inactiveConnectors > 0 ? "Offline: " + inactiveConnectors : "Alles Online";
                            const infoTerminals = inactiveTerminals > 0 ? "Offline: " + inactiveTerminals : "Alles Online";
-                           const cardData = data[2];
-                           let content = [];
-                           content.push({
+                           const connectorData = data[2];
+                           const cardData = data[4];
+                           const statusSMCB = invalidSMCB > 0 ? "Error" : "Success";
+                           const statusHBA = invalidHBA > 0 ? "Error" : "Success";
+                           const infoSMCB = invalidSMCB > 0 ? "Pin-Status: " + invalidSMCB + " nicht verifiziert" : "Pin-Status: Alles verifiziert";
+                           const infoHBA = invalidHBA > 0 ? "Pin-Status: " + invalidHBA + " nicht verifiziert" : "Pin-Status: Alles verifiziert";
+                           let connectorContent = [];
+                           let cardContent = [];
+                           connectorContent.push({
                              "Name" : "Konnektoren",
                              "Value": configs.length,
                              "Icon": url + "dashboard/images/Connector.png",
                              "State" : statusConnectors,
                              "Info": infoConnectors
                            });
-                           content.push({
+                           connectorContent.push({
                              "Name" : "Kartenterminals",
                              "Value" : numTerminals,
                              "Icon": url + "dashboard/images/CardTerminal.png",
                              "State": statusTerminals,
                              "Info" : infoTerminals
                            });
-                           content.push({
+                           connectorContent.push({
                              "Name" : "Karten",
                              "Value": numCards,
                              "Icon": url + "dashboard/images/Card.png",
                              "State": "None"
                            });
-                           cardData.connectors["sap.card"].content.data.json = content;
-                           oConnectorList.setData(cardData);
+                           cardContent.push({
+                             "Name" : "SMC-KT",
+                             "Value": cardTypes["SMC_KT"],
+                             "Info": "Pin-Status: Alle unbekannt   " + "Abgelaufen: 0",
+                             "State": "Success"
+                           });
+                           cardContent.push({
+                             "Name": "SMC-B",
+                             "Value": cardTypes["SMC_B"],
+                             "Info": infoSMCB + "   Abgelaufen: 0",
+                             "State": statusSMCB
+                           });
+                           cardContent.push({
+                             "Name": "HBA",
+                             "Value": cardTypes["HBA"],
+                             "Info": infoHBA + "    Abgelaufen: 0",
+                             "State": statusHBA
+                           });
+                           cardContent.push({
+                             "Name": "EGK",
+                             "Value": cardTypes["EGK"],
+                             "Info": "Pin-Status: Alle unbekannt   " + "Abgelaufen: 0",
+                             "State": "Success"
+                           });
+                           cardContent.push({
+                             "Name": "KVK",
+                             "Value": cardTypes["KVK"],
+                             "Info": "Pin-Status: Alle unbekannt   " + "Abgelaufen: 0",
+                             "State": "Success"
+                           });
+                           console.log(cardContent);
+                           connectorData.connectors["sap.card"].content.data.json = connectorContent;
+                           oConnectorList.setData(connectorData);
+                           cardData.cards["sap.card"].content.data.json = cardContent;
+                           console.log(cardData);
+                           oCardList.setData(cardData);
                         }
                      });
                    });
