9231bc2b56fcf04824a70830da60e11c809bf8dc
simoneS96 <simone.stifano@incentergy.de>
2023-05-03 15:43:55 +0200

    use the client certificate to shut down connector

--- src/main/java/health/medunited/architecture/jaxrs/management/SecunetConnector.java
+++ src/main/java/health/medunited/architecture/jaxrs/management/SecunetConnector.java
@@ -1,8 +1,10 @@
 package health.medunited.architecture.jaxrs.management;
 
+import health.medunited.architecture.service.common.security.SecretsManagerService;
 import health.medunited.architecture.service.endpoint.SSLUtilities;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
@@ -19,35 +21,25 @@ import java.util.logging.Logger;
 
 @ApplicationScoped
 @Named("secunet")
-public class SecunetConnector implements Connector{
+public class SecunetConnector implements Connector {
 
     private static final Logger log = Logger.getLogger(SecunetConnector.class.getName());
+
+    @Inject
+    SecretsManagerService secretsManagerService;
 
     @Override
     public void restart(String connectorUrl, String managementPort) {
         log.log(Level.INFO, "Restarting Secunet connector");
 
-        //TODO: This is only a test to prove that it works. We have to change it with security in mind and pass the parameters from the HTTP Request
         ClientBuilder clientBuilder = ClientBuilder.newBuilder();
 
         clientBuilder.connectTimeout(3, TimeUnit.SECONDS);
         clientBuilder.readTimeout(3, TimeUnit.SECONDS);
 
-        clientBuilder.hostnameVerifier(new SSLUtilities.FakeHostnameVerifier());
+        SSLContext sslContext = secretsManagerService.getSslContext();
 
-        // Create a TrustManager that trusts all certificates
-        TrustManager[] trustAllCerts = setupTrustManager();
-
-        // Configure the client to use the TrustManager
-        SSLContext sslContext;
-        try {
-            sslContext = SSLContext.getInstance("SSL");
-            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-        } catch (KeyManagementException | NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
         clientBuilder.sslContext(sslContext);
-        clientBuilder.hostnameVerifier((hostname, session) -> true);
 
         Client client = clientBuilder.build();
 
@@ -60,31 +52,13 @@ public class SecunetConnector implements Connector{
         restartBuilder.post(Entity.json(""));
     }
 
-    private TrustManager[] setupTrustManager() {
-        return new TrustManager[]{
-                new X509TrustManager() {
-                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                        return new X509Certificate[0];
-                    }
-
-                    public void checkClientTrusted(
-                            java.security.cert.X509Certificate[] certs, String authType) {
-                    }
-
-                    public void checkServerTrusted(
-                            java.security.cert.X509Certificate[] certs, String authType) {
-                    }
-                }
-        };
-    }
-
     private Response loginManagementConsole(Client client, String connectorUrl, String managementPort) {
         WebTarget loginTarget = client.target(connectorUrl + ":" + managementPort)
                 .path("/rest/mgmt/ak/konten/login");
 
         Invocation.Builder loginBuilder = loginTarget.request(MediaType.APPLICATION_JSON);
         return loginBuilder.post(Entity.json("{\n" +
-                "    \"username\": \"fakeusername\",\n" +
+                "    \"username\": \"fakeuser\",\n" +
                 "    \"password\": \"fakepassword\"\n" +
                 "}"));
     }
