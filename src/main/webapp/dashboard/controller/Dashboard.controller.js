5abbc51b50902e4c2781a82c1c8f2279a16a1603
TimonWag <97670643+TimonWag@users.noreply.github.com>
2023-05-03 10:23:45 +0200

    CAT-75 Added return button for management page

--- src/main/webapp/dashboard/controller/Dashboard.controller.js
+++ src/main/webapp/dashboard/controller/Dashboard.controller.js
@@ -181,6 +181,9 @@ sap.ui.define(
                  },
                });
         },
+        onBeforeRendering: function () {
+         this.onInit();
+        },
 
         onRouteToMaster: function (oEvent) {
           this.oRouter.navTo("master");
