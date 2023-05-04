b4c9d2a7f4a86388a9b319a77367c577aafe7d6c
simoneS96 <102789473+simoneS96@users.noreply.github.com>
2023-05-04 09:53:04 +0200

    CAT-74-WIP: restart connectors based on brand (#22)
    * Interfaces and implementation of different connectors
    
    * add management console url + post in http request
    
    * CAT-74: split into private methods
    
    * CAT-74: remove duplicated model package
    
    * change kops url
    
    * timeout on restart client
    
    * use the client certificate to shut down connector
    
    * remove imports
    
    * finish restart of the connector functionality

---
+++ src/main/java/health/medunited/architecture/jaxrs/resource/ConnectorManagement.java
@@ -0,0 +1,55 @@
+package health.medunited.architecture.jaxrs.resource;
+
+import health.medunited.architecture.jaxrs.management.Connector;
+import health.medunited.architecture.jaxrs.management.ConnectorBrands;
+import health.medunited.architecture.model.RestartRequestBody;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.Map;
+
+@Path("management")
+public class ConnectorManagement {
+
+    @Inject
+    @Named("secunet")
+    private Connector secunetConnector;
+
+    @Inject
+    @Named("kocobox")
+    private Connector kocoboxConnector;
+
+    @Inject
+    @Named("rise")
+    private Connector riseConnector;
+
+    private Map<String, Connector> connectorMap;
+
+    @PostConstruct
+    public void init() {
+        this.connectorMap = new HashMap<>();
+        connectorMap.put(ConnectorBrands.SECUNET.getValue(), secunetConnector);
+        connectorMap.put(ConnectorBrands.KOCOBOX.getValue(), kocoboxConnector);
+        connectorMap.put(ConnectorBrands.RISE.getValue(), riseConnector);
+    }
+
+    @POST
+    @Path("/{connectorType}/restart")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void restart(@PathParam("connectorType") String connectorType,
+                        @QueryParam("connectorUrl") String connectorUrl,
+                        @QueryParam("managementPort") String managementPort,
+                        RestartRequestBody managementCredentials
+    ) {
+
+        Connector connector = connectorMap.get(connectorType);
+        if (connector == null) {
+            throw new IllegalArgumentException("Unknown connector type: " + connectorType);
+        }
+        connector.restart(connectorUrl, managementPort, managementCredentials);
+    }
+}
