5d1904a4d987640856f2aebffa4785a9540e02da
István Orosz <ruszkipista@users.noreply.github.com>
2023-05-09 23:12:20 +0100

    restore Version.java

--- src/main/java/health/medunited/architecture/jaxrs/resource/Version.java
+++ src/main/java/health/medunited/architecture/jaxrs/resource/Version.java
@@ -1,50 +1,28 @@
 package health.medunited.architecture.jaxrs.resource;
-package health.medunited.architecture.jaxrs.resource;
-
-
-import health.medunited.architecture.service.endpoint.EndpointDiscoveryService;
-import java.util.logging.Logger;
-
-
-import javax.enterprise.context.RequestScoped;
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
-import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.Path;
-import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response;
-import java.util.logging.Logger;
 
 import health.medunited.architecture.service.endpoint.EndpointDiscoveryService;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import java.util.logging.Logger;
 
 @RequestScoped
-@RequestScoped
-@Path("productTypeInformation")
 @Path("productTypeInformation")
 public class Version {
-public class Version {
+
     private static final Logger log = Logger.getLogger(Version.class.getName());
-    private static final Logger log = Logger.getLogger(Version.class.getName());
-    @Inject
+
     @Inject
     EndpointDiscoveryService endpointDiscoveryService;
-    EndpointDiscoveryService endpointDiscoveryService;
-    @GET
+
     @GET
     @Path("/getVersion")
-    @Path("/getVersion")
-    public Response getProductTypeInformation(@HeaderParam("X-Host") String connectorUrl) {
     public Response getProductTypeInformation(@HeaderParam("X-Host") String connectorUrl) {
         endpointDiscoveryService.obtainConfiguration(connectorUrl);
-        endpointDiscoveryService.obtainConfiguration(connectorUrl);
         return Response.ok(endpointDiscoveryService.getConnectorVersion()).build();
-        return Response.ok(endpointDiscoveryService.getConnectorVersion()).build();
-    }
     }
 }
