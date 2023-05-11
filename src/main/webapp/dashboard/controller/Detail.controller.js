f0892ccc129fe6c47bd312e551705cd878575b57
Pablo Escaida <34131550+pablitomilenio@users.noreply.github.com>
2023-05-11 16:19:58 +0200

    CAT-117 adjust endpoint called to the connector type (#48)
    * CAT-117: Fetching the right device type and restarting with the correct endpoint
    
    * CAT-117: rearranged the restart functions to load after the data-load functions in the detail screen
    
    * CAT-117 Adjust endpoint called to the connector type
    
    ---------
    
    Co-authored-by: ruszkipista <oi5672@gmail.com>

--- src/main/webapp/dashboard/controller/Detail.controller.js
+++ src/main/webapp/dashboard/controller/Detail.controller.js
@@ -180,60 +180,6 @@ sap.ui.define(
           }
         },
 
-        pwdOnCancel: function (oEvent) {
-          oEvent.getSource().getParent().close();
-          oEvent.getSource().getParent().destroy();
-        },
-
-        pwdOnRestart: function (oEvent) {
-          const sPath = "/RuntimeConfigs('" + this._entity + "')";
-          const oRuntimeConfig = this.getView().getModel().getProperty(sPath);
-          const restartHeaders = {
-            "x-client-certificate": oRuntimeConfig.ClientCertificate,
-            "x-client-certificate-password":
-              oRuntimeConfig.ClientCertificatePassword,
-            "Content-Type": "application/json",
-          };
-          const username = this.byId("usernameInput").getValue();
-          const password = this.byId("passwordInput").getValue();
-          const requestBody = {
-            username: username,
-            password: password,
-          };
-          fetch(
-            "connector/management/secunet/restart?connectorUrl=" +
-              oRuntimeConfig.Url +
-              "&managementPort=8500",
-            {
-              headers: restartHeaders,
-              method: "POST",
-              body: JSON.stringify(requestBody),
-            }
-          );
-          oEvent.getSource().getParent().close();
-          MessageToast.show(this.translate("restarting"));
-          oEvent.getSource().getParent().destroy();
-        },
-
-        restartConnector: function () {
-          let oView = this.getView();
-          const me = this;
-
-          if (!this.byId("RestartPasswordDialog")) {
-            Fragment.load({
-              id: oView.getId(),
-              name: "sap.f.ShellBarWithFlexibleColumnLayout.view.RestartPasswordDialog",
-              controller: this,
-            }).then(function (oDialog) {
-              me.onAfterCreateOpenDialog({ dialog: oDialog });
-              oView.addDependent(oDialog);
-              me._openCreateDialog(oDialog);
-            });
-          } else {
-            this._openCreateDialog(this.byId("restartPasswordDialog"));
-          }
-        },
-
         reloadModels: function (oRuntimeConfig) {
           this.handleFullScreen();
 
@@ -365,10 +311,10 @@ sap.ui.define(
             });
 
           fetch("connector/sds/config", { headers: mHeaders })
-          .then((re) => re.json())
-          .then((remoteConfig) => {
+            .then((re) => re.json())
+            .then((remoteConfig) => {
               this.getView().getModel("ConnectorSDS").setData(remoteConfig);
-          });
+            });
         },
 
         getEntityName: function () {
@@ -463,6 +409,87 @@ sap.ui.define(
             "x-user-id": oRuntimeConfig.UserId,
             "x-host": oRuntimeConfig.Url,
           };
+        },
+
+        _getConnectorType: async function () {
+          const mHeaders = this._getHttpHeadersFromRuntimeConfig();
+
+          let promise = new Promise((resolve) => {
+            fetch("connector/sds/config", { headers: mHeaders })
+              .then((remoteResponse) => remoteResponse.json())
+              .then((remoteConfig) => {
+                let productCode =
+                  remoteConfig.productInformation.productIdentification
+                    .productCode;
+                let connectorBrand;
+                if (productCode == "secu_kon") connectorBrand = "secunet";
+                else if (productCode == "RKONN") connectorBrand = "rise";
+                else if (productCode == "kocobox") connectorBrand = "kocobox";
+                resolve(connectorBrand);
+              });
+          });
+
+          return await promise;
+        },
+
+        pwdOnCancel: function (oEvent) {
+          oEvent.getSource().getParent().close();
+          oEvent.getSource().getParent().destroy();
+        },
+
+        restartConnector: function () {
+          let oView = this.getView();
+          const me = this;
+
+          if (!this.byId("RestartPasswordDialog")) {
+            Fragment.load({
+              id: oView.getId(),
+              name: "sap.f.ShellBarWithFlexibleColumnLayout.view.RestartPasswordDialog",
+              controller: this,
+            }).then(function (oDialog) {
+              me.onAfterCreateOpenDialog({ dialog: oDialog });
+              oView.addDependent(oDialog);
+              me._openCreateDialog(oDialog);
+            });
+          } else {
+            this._openCreateDialog(this.byId("restartPasswordDialog"));
+          }
+        },
+
+        pwdOnRestart: function (oEvent) {
+          const sPath = "/RuntimeConfigs('" + this._entity + "')";
+          const oRuntimeConfig = this.getView().getModel().getProperty(sPath);
+          const restartHeaders = {
+            "x-client-certificate": oRuntimeConfig.ClientCertificate,
+            "x-client-certificate-password":
+              oRuntimeConfig.ClientCertificatePassword,
+            "Content-Type": "application/json",
+          };
+          const username = this.byId("usernameInput").getValue();
+          const password = this.byId("passwordInput").getValue();
+          const requestBody = {
+            username: username,
+            password: password,
+          };
+
+          oEvent.getSource().getParent().close();
+          MessageToast.show(this.translate("restarting"));
+          oEvent.getSource().getParent().destroy();
+
+          this._getConnectorType().then((deviceType) => {
+            fetch(
+              "connector/management/" +
+                deviceType +
+                "/restart?connectorUrl=" +
+                oRuntimeConfig.Url +
+                "&managementPort=8500",
+              {
+                headers: restartHeaders,
+                method: "POST",
+                body: JSON.stringify(requestBody),
+              }
+            );
+          });
         },
       }
     );
