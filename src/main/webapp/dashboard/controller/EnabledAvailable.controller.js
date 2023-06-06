bd1d50eb09adb51e53256b2cf3b14950cfba839d
Pablo Escaida <1pablo.escaida1@gmail.com>
2023-06-06 17:27:29 +0200

    CAT-113 fixes route to new view

--- src/main/webapp/dashboard/controller/EnabledAvailable.controller.js
+++ src/main/webapp/dashboard/controller/EnabledAvailable.controller.js
@@ -1,5 +1,14 @@
 sap.ui.define([
-"sap/ui/core/mvc/Controller"
-], function (Controller) {
-    "use strict";
-}, true);
+    "sap/ui/core/mvc/Controller"
+    ], function (Controller) {
+        "use strict";
+    
+        return Controller.extend("sap.f.ShellBarWithFlexibleColumnLayout.controller.AbstractController", {
+            translate: function (sKey, aArgs, bIgnoreKeyFallback) {
+                return (sKey)
+                        ? this.getOwnerComponent().getModel("i18n").getResourceBundle().getText(sKey, aArgs, bIgnoreKeyFallback)
+                        : '';
+            }
+        });
+    }, true);
+    
