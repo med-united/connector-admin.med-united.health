5393c6de67765453fac7ca38d348e980b93924a3
Pablo Escaida <1pablo.escaida1@gmail.com>
2023-06-07 15:35:56 +0200

    CAT-113 adds a new controller prototype

--- src/main/webapp/dashboard/controller/EnabledAvailable.controller.js
+++ src/main/webapp/dashboard/controller/EnabledAvailable.controller.js
@@ -1,14 +1,20 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-    ], function (Controller) {
-        "use strict";
-    
-        return Controller.extend("sap.f.ShellBarWithFlexibleColumnLayout.controller.AbstractController", {
-            translate: function (sKey, aArgs, bIgnoreKeyFallback) {
-                return (sKey)
-                        ? this.getOwnerComponent().getModel("i18n").getResourceBundle().getText(sKey, aArgs, bIgnoreKeyFallback)
-                        : '';
-            }
-        });
-    }, true);
-    
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/dnd/DragInfo",
+	"sap/ui/core/dnd/DropInfo",
+	"sap/f/dnd/GridDropInfo",
+	"sap/ui/core/library"
+], function (Controller, JSONModel, DragInfo, DropInfo, GridDropInfo, coreLibrary) {
+	"use strict";
+
+	// shortcut for sap.ui.core.dnd.DropLayout
+	var DropLayout = coreLibrary.dnd.DropLayout;
+
+	// shortcut for sap.ui.core.dnd.DropPosition
+	var DropPosition = coreLibrary.dnd.DropPosition;
+
+	return Controller.extend("sap.f.sample.GridContainerDragAndDropFromList.C", {
+
+    })
+});
