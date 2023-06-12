05d2ed5ec582b6a4f53a3b88174f6bbb0d0f647c
Pablo Escaida <34131550+pablitomilenio@users.noreply.github.com>
2023-06-12 13:03:23 +0200

    KAT-141 remove fullscreen setting when opening the connector's details page (#54)
    * CAT-141 removes the fullscreen mode
    
    * CAT-141 Optional Fullscreen implemented

--- src/main/webapp/dashboard/controller/AbstractDetailController.js
+++ src/main/webapp/dashboard/controller/AbstractDetailController.js
@@ -1,88 +1,125 @@
-sap.ui.define([
-	"./AbstractController",
-	'sap/m/MessageBox',
-	'sap/m/MessageToast',
-	"sap/ui/core/Fragment"
-], function (AbstractController, MessageBox, MessageToast, Fragment) {
-	"use strict";
+sap.ui.define(
+  [
+    "./AbstractController",
+    "sap/m/MessageBox",
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment",
+  ],
+  function (AbstractController, MessageBox, MessageToast, Fragment) {
+    "use strict";
 
-	return AbstractController.extend("sap.f.ShellBarWithFlexibleColumnLayout.controller.AbstractDetailController", {
-		onInit: function () {
-			this.oRouter = this.getOwnerComponent().getRouter();
+    return AbstractController.extend(
+      "sap.f.ShellBarWithFlexibleColumnLayout.controller.AbstractDetailController",
+      {
+        onInit: function () {
+          this.oRouter = this.getOwnerComponent().getRouter();
 
-			this.oRouter.getRoute("detail").attachPatternMatched(this._onMatched, this);
-		},
-		handleFullScreen: function () {
-			this.navToLayoutProperty("/actionButtonsInfo/midColumn/fullScreen");
-		},
+          this.oRouter
+            .getRoute("detail")
+            .attachPatternMatched(this._onMatched, this);
+        },
         onAfterCreateOpenDialog: function () {},
         _openCreateDialog: function (oDialog, sEntityName) {
-                    oDialog.open();
+          oDialog.open();
         },
-		navToLayoutProperty: function (sLayoutProperty) {
-			let oLayoutModel = this.getOwnerComponent().getModel("Layout");
-			let sNextLayout = oLayoutModel.getProperty(sLayoutProperty);
-			if (sNextLayout == null) sNextLayout = "MidColumnFullScreen";
-			let oParams = { layout: sNextLayout };
-			oParams["id"] = this._entity;
-			this.oRouter.navTo("detail", oParams);
-		},
-		handleClose: function () {
-			let oLayoutModel = this.getOwnerComponent().getModel("Layout");
-			let sNextLayout = oLayoutModel.getProperty("/actionButtonsInfo/midColumn/closeColumn");
-			this.oRouter.navTo("master", { layout: sNextLayout });
-		},
-		onEdit: function () {
-			this.enableEditMode(true);
-		},
-		onSave: function (oEvent) {
-			let oModel = this.getView().getModel();
-			if (this.validateResource()) {
-				let fnSuccess = function (oData) {
-					this.enableEditMode(false);
-					MessageToast.show(this.translate(this.getEntityName()) + ' ' + this.translate("msgSaveResourceSuccessful"));
-				}.bind(this);
+        navToLayoutProperty: function (sLayoutProperty) {
+          let oLayoutModel = this.getOwnerComponent().getModel("Layout");
+          let sNextLayout = oLayoutModel.getProperty(sLayoutProperty);
+          if (sNextLayout == null) sNextLayout = "MidColumnFullScreen";
+          let oParams = { layout: sNextLayout };
+          oParams["id"] = this._entity;
+          this.oRouter.navTo("detail", oParams);
+        },
+        handleClose: function () {
+          let oLayoutModel = this.getOwnerComponent().getModel("Layout");
+          let sNextLayout = oLayoutModel.getProperty(
+            "/actionButtonsInfo/midColumn/closeColumn"
+          );
+          this.oRouter.navTo("master", { layout: sNextLayout });
+        },
+        onEdit: function () {
+          this.enableEditMode(true);
+        },
+        handleFullScreen: function () {
+          this.navToLayoutProperty("/actionButtonsInfo/midColumn/fullScreen");
+        },
+        handleExitFullScreen: function () {
+          this.navToLayoutProperty(
+            "/actionButtonsInfo/midColumn/exitFullScreen"
+          );
+        },
+        onSave: function (oEvent) {
+          let oModel = this.getView().getModel();
+          if (this.validateResource()) {
+            let fnSuccess = function (oData) {
+              this.enableEditMode(false);
+              MessageToast.show(
+                this.translate(this.getEntityName()) +
+                  " " +
+                  this.translate("msgSaveResourceSuccessful")
+              );
+            }.bind(this);
 
-				let fnError = function (oError) {
-					this.enableEditMode(false);
-					MessageBox.show(this.translate(this.getEntityName()) + ' ' + this.translate("msgSaveResouceFailed", [oError.statusCode, oError.statusText]));
-				}.bind(this);
+            let fnError = function (oError) {
+              this.enableEditMode(false);
+              MessageBox.show(
+                this.translate(this.getEntityName()) +
+                  " " +
+                  this.translate("msgSaveResouceFailed", [
+                    oError.statusCode,
+                    oError.statusText,
+                  ])
+              );
+            }.bind(this);
 
-				let oRequest = oModel.submitChanges(this.getEntityName().toLowerCase() + "Details", fnSuccess, fnError);
-				if (!oRequest) {
-					this.enableEditMode(false);
-				}
-			}
-		},
-		validateResource: function () {
-			return true;
-		},
-		onCancel: function (oEvent) {
-			this.enableEditMode(false);
-			this.getView().getModel().resetChanges();
-		},
-		onDelete: function (oEvent) {
-			this.getOwnerComponent().getRouter().navTo("master");
-		},
-		_onMatched: function (oEvent) {
-			this._entity = oEvent.getParameter("arguments")["id"];
-			this.getView().bindElement({
-				path: "/" + this.getEntityName() + "s('" + this._entity+"')",
-				parameters: this.getBindElementParams()
-			});
-		},
-		getBindElementParams: function () {
-			return {};
-		},
-		getEntityName: function () {
-			throw new Error("getEntityName must be implemented by derived class");
-		},
-		onExit: function () {
-			this.oRouter.getRoute(this.getEntityName().toLowerCase() + "-master").detachPatternMatched(this._onMatched, this);
-			this.oRouter.getRoute(this.getEntityName().toLowerCase() + "-detail").detachPatternMatched(this._onMatched, this);
-		},
-		enableEditMode: function (bEditMode) {
-			this.getView().getModel("appState").setProperty("/editMode", bEditMode);
-		}
-	});
-}, true);
+            let oRequest = oModel.submitChanges(
+              this.getEntityName().toLowerCase() + "Details",
+              fnSuccess,
+              fnError
+            );
+            if (!oRequest) {
+              this.enableEditMode(false);
+            }
+          }
+        },
+        validateResource: function () {
+          return true;
+        },
+        onCancel: function (oEvent) {
+          this.enableEditMode(false);
+          this.getView().getModel().resetChanges();
+        },
+        onDelete: function (oEvent) {
+          this.getOwnerComponent().getRouter().navTo("master");
+        },
+        _onMatched: function (oEvent) {
+          this._entity = oEvent.getParameter("arguments")["id"];
+          this.getView().bindElement({
+            path: "/" + this.getEntityName() + "s('" + this._entity + "')",
+            parameters: this.getBindElementParams(),
+          });
+        },
+        getBindElementParams: function () {
+          return {};
+        },
+        getEntityName: function () {
+          throw new Error("getEntityName must be implemented by derived class");
+        },
+        onExit: function () {
+          this.oRouter
+            .getRoute(this.getEntityName().toLowerCase() + "-master")
+            .detachPatternMatched(this._onMatched, this);
+          this.oRouter
+            .getRoute(this.getEntityName().toLowerCase() + "-detail")
+            .detachPatternMatched(this._onMatched, this);
+        },
+        enableEditMode: function (bEditMode) {
+          this.getView()
+            .getModel("appState")
+            .setProperty("/editMode", bEditMode);
+        },
+      }
+    );
+  },
+  true
+);
