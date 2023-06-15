25efdb8a4df9d2ddf23e187fc6f053aeed10522b
Pablo Escaida <1pablo.escaida1@gmail.com>
2023-06-15 16:09:53 +0200

    CAT 157 compares the current version to the newest and displays it in the test

--- src/main/java/health/medunited/architecture/jaxrs/management/SecunetConnector.java
+++ src/main/java/health/medunited/architecture/jaxrs/management/SecunetConnector.java
@@ -11,8 +11,8 @@ import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.json.*;
 
-import com.google.gson.JsonObject;
 import health.medunited.architecture.model.ManagementCredentials;
 
 @ApplicationScoped
@@ -52,23 +52,42 @@ public class SecunetConnector extends AbstractConnector {
 
     public void checkUpdate(String connectorUrl, String managementPort, ManagementCredentials managementCredentials) {
         log.log(Level.INFO, "Checking the Update Version of the Secunet Connector");
+        System.out.println(" ");
 
         Client client = buildClient();
 
         Response loginResponse = loginManagementConsole(client, connectorUrl, managementPort, managementCredentials);
 
 
-        WebTarget updateTarget = client.target(connectorUrl + ":" + managementPort)
+        // get current firmware Version
+        WebTarget fwTarget = client.target(connectorUrl + ":" + managementPort)
                 .path("/rest/mgmt/ak/dienste/status/version");
-        Invocation.Builder updateBuilder = updateTarget.request(MediaType.APPLICATION_JSON);
-        updateBuilder.header("Authorization", loginResponse.getHeaders().get("Authorization").get(0));
-        Response updateResponse = updateBuilder.get();
+        Invocation.Builder fwBuilder = fwTarget.request(MediaType.APPLICATION_JSON);
+        fwBuilder.header("Authorization", loginResponse.getHeaders().get("Authorization").get(0));
+        Response fwResponse = fwBuilder.get();
 
-        String log2 = updateResponse.readEntity(String.class);
-        //String log3 = log2.getAsString();
+        String firmwareJson = fwResponse.readEntity(String.class);
 
-        System.out.println("l2:"+log2);
-        //System.out.println("l3:"+log3);
+        JSONObject jsonObject = new JSONObject(firmwareJson);
+        String fwVersion = jsonObject.getString("fwVersion");
+        System.out.println("Currently installed firmware version: "+fwVersion);
+
+        //get latest possible Update version
+        WebTarget updatesTarget = client.target(connectorUrl + ":" + managementPort)
+                .path("/rest/mgmt/ak/dienste/ksr/informationen/updates-konnektor");
+        Invocation.Builder updatesBuilder = updatesTarget.request(MediaType.APPLICATION_JSON);
+        updatesBuilder.header("Authorization", loginResponse.getHeaders().get("Authorization").get(0));
+        Response updatesResponse = updatesBuilder.get();
+
+        String updatesJson = updatesResponse.readEntity(String.class);
+        int versionPosition = updatesJson.lastIndexOf("Release der Version ");
+
+        String latestVersion = updatesJson.substring(versionPosition+20,updatesJson.indexOf('"',versionPosition+2));
+
+        System.out.println("Latest possible update version: "+latestVersion);
+        System.out.println(" ");
+        if (fwVersion == latestVersion) System.out.println("Connector updated to latest version: true");
+        else System.out.println("Connector updated to latest version: false");
 
     }
 }
