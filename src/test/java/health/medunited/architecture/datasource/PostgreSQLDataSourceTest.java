b51c1ba9d098ef808c000482cab9bb06c90b9cb4
BeatrizRCorreia <beatriz.correia.pt@gmail.com>
2023-09-04 21:08:38 +0100

    KAT-192 add tests to the connection to a local postgreSQL db

---
+++ src/test/java/health/medunited/architecture/datasource/PostgreSQLDataSourceTest.java
@@ -0,0 +1,63 @@
+package health.medunited.architecture.datasource;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.postgresql.ds.PGSimpleDataSource;
+
+import javax.sql.DataSource;
+import java.sql.*;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class PostgreSQLDataSourceTest {
+
+    private DataSource dataSource;
+    private Connection connection;
+
+    @Before
+    public void setUp() throws SQLException {
+        PGSimpleDataSource pgDataSource = new PGSimpleDataSource();
+        pgDataSource.setServerNames(new String[]{"localhost"});
+        pgDataSource.setPortNumbers(new int[]{5432});
+        pgDataSource.setDatabaseName("postgres");
+        pgDataSource.setUser("postgres");
+        pgDataSource.setPassword("mysecretpassword");
+
+        dataSource = pgDataSource;
+
+        connection = dataSource.getConnection();
+    }
+
+    @After
+    public void tearDown() throws SQLException {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    @Test
+    public void testDatabaseConnection() throws SQLException {
+        assertNotNull("DataSource should not be null", dataSource);
+        assertNotNull("Connection should not be null", connection);
+
+        // Execute a simple query to test the connection
+        String sql = "SELECT 1";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+            assertTrue("Query should return a result", resultSet.next());
+        }
+    }
+
+    @Test
+    public void testRuntimeConfigTableExists() throws SQLException {
+        assertNotNull("DataSource should not be null", dataSource);
+        assertNotNull("Connection should not be null", connection);
+
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet resultSet = metaData.getTables(null, null, "runtimeconfig", new String[]{"TABLE"});
+
+        assertTrue("Table 'runtimeconfig' should exist", resultSet.next());
+    }
+}
